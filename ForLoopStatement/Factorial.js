13f9bde3fa9c894e7403c0a93d307c826bbdd87d
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-03-01 13:16:39 +0530

    [Kapil_namdeo]Add. Factorialjavascript  program

---
+++ ForLoopStatement/Factorial.js
@@ -0,0 +1,16 @@
+const num = parseInt(process.argv[2], 10); // Read input from command line
+
+if (isNaN(num) || num < 0) {
+    console.log("Please enter a valid non-negative integer.");
+} else {
+    function factorial(n) {
+        if (n === 0 || n === 1) return 1;
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    console.log(`${num}! = ${factorial(num)}`);
+}
